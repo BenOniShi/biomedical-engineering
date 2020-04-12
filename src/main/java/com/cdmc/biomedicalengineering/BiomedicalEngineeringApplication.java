38b4c00685d288d85b83d80fce8a97c327beb2a9
BenOnShi <shibn9510@gmail.com>
2020-04-12 15:54:36 +0800

    创建仓库

---
+++ src/main/java/com/cdmc/biomedicalengineering/BiomedicalEngineeringApplication.java
@@ -0,0 +1,16 @@
+package com.cdmc.biomedicalengineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sbn
+ */
+@SpringBootApplication
+public class BiomedicalEngineeringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiomedicalEngineeringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cdmc/biomedicalengineering/BiomedicalEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdmc.biomedicalengineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiomedicalEngineeringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
